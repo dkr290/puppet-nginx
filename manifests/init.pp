7591af549c68192c9a07cdeacf8c89b84f89a83c
root <root@puppet.cbtlux.int>
2018-06-22 13:30:37 +0200

    full module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,34 @@
-# Class: nginx
-# ===========================
-#
-# Full description of class nginx here.
-#
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#  Explanation of how this variable affects the function of this class and if
-#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#  External Node Classifier as a comma separated list of hostnames." (Note,
-#  global variables should be avoided in favor of class parameters as
-#  of Puppet 2.6.)
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'nginx':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2018 Your name here, unless otherwise noted.
-#
-class nginx {
+class nginx(
+
+
+$package_name,
+$config_dir,
+$config_ensure,
+$config_mode,
+$config_owner,
+$config_group,
+$config_confd,
+$config_log_dir,
+$config_pid_file,
+$service_name,
+$service_ensure,
+$service_pattern,
+$service_enable,
+$service_hasstatus,
+$service_hasrestart,
+$docroot,
+$config_vdir_enable,
+$config_process_user,
+$vhost_dir,
+
+
+) {
+
+class {'::nginx::install':}
+-> class {'::nginx::config':
+     notify => Class['::nginx::service'],
+  }
+  class {'::nginx::service':}
 
 
 }
