dfe7a452669af32779ceb5c3e300fb0d6aa3b4e6
root <root@puppet.cbtlux.int>
2018-06-22 13:33:02 +0200

    full module1

---
+++ functions/params.pp
@@ -0,0 +1,44 @@
+function nginx::params(
+  Hash  $options,
+  Puppet::LookupContext  $context,
+){
+    $base_params = {
+
+       'nginx::ensure'           => 'present',
+       'nginx::package_name'     => 'nginx',
+       'nginx::config_dir'       =>  '/etc/nginx',
+       'nginx::config_ensure'    => 'file',
+       'nginx::config_mode'      => '0664',
+       'nginx::config_owner'     => 'root',
+       'nginx::config_group'     => 'root',
+       'nginx::config_confd'     => "${nginx::config_dir}/conf.d",
+       'nginx::config_log_dir'   => '/var/log/nginx',
+       'nginx::config_pid_file'  => '/run/nginx.pid',
+      
+       'nginx::service_name'       => 'nginx',
+       'nginx::service_ensure'     => 'running',
+       'nginx::service_pattern'    => 'nginx',
+       'nginx::service_enable'     =>  true,
+       'nginx::service_hasstatus'  =>  true,
+       'nginx::service_hasrestart' =>  true,
+       'nginx::docroot'            =>  '/usr/share/nginx',
+      }
+
+    $os_params  = case $facts['os']['family'] {
+           'Debian': {
+                { 'nginx::config_vdir_enable'   => '/etc/nginx',
+                'nginx::config_process_user'  => 'www-data',
+                'nginx::vhost_dir'            => '/etc/nginx/sites-enabled' }
+             }
+            default: { 
+                { 'nginx::config_vdir_enable'  => undef,
+                 'nginx::config_process_user' => 'nginx', 
+                 'nginx::vhost_dir'           => '/etc/nginx' }
+             }
+       
+        }
+    $base_params  + $os_params
+
+
+}
+
---
+++ manifests/params.pp
@@ -0,0 +1,10 @@
+class nginx::params {
+
+  $ensure        = 'present'
+  $packageneme   = 'nginx'
+  $config_dir    = '/etc/nginx/'
+  $config_ensure = 'file' 
+
+
+
+}
