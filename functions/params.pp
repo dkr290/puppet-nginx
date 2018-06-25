7b576c689d8c5f65397d3a9d42a28b39c238b7d8
root <root@puppet.cbtlux.int>
2018-06-25 11:50:07 +0200

    latest bug fixing

--- functions/params.pp
+++ functions/params.pp
@@ -5,13 +5,12 @@ function nginx::params(
     $base_params = {
 
        'nginx::ensure'           => 'present',
-       'nginx::package_name'     => 'nginx',
        'nginx::config_dir'       =>  '/etc/nginx',
        'nginx::config_ensure'    => 'file',
        'nginx::config_mode'      => '0664',
        'nginx::config_owner'     => 'root',
        'nginx::config_group'     => 'root',
-       'nginx::config_confd'     => "${nginx::config_dir}/conf.d",
+       'nginx::config_confd'     => "/etc/nginx/conf.d",
        'nginx::config_log_dir'   => '/var/log/nginx',
        'nginx::config_pid_file'  => '/run/nginx.pid',
       
@@ -28,12 +27,14 @@ function nginx::params(
            'Debian': {
                 { 'nginx::config_vdir_enable'   => '/etc/nginx',
                 'nginx::config_process_user'  => 'www-data',
-                'nginx::vhost_dir'            => '/etc/nginx/sites-enabled' }
+                'nginx::vhost_dir'            => '/etc/nginx/sites-enabled',
+                'nginx::package_name'         => 'nginx-full' }
              }
             default: { 
                 { 'nginx::config_vdir_enable'  => undef,
                  'nginx::config_process_user' => 'nginx', 
-                 'nginx::vhost_dir'           => '/etc/nginx' }
+                 'nginx::vhost_dir'           => '/etc/nginx',
+                 'nginx::package_name'         => 'nginx'    }
              }
        
         }
